adabb4e4349d3bcde3739be31f48d8c7043b6d3a
Jude Niroshan <jude.niroshan11@gmail.com>
2020-03-08 20:44:08 +0100

    service registration for eureka server (#7)

--- src/config.js
+++ src/config.js
@@ -1,6 +1,17 @@
 const config = {};
 
-config.persist = {};
-config.persist.type = 'file';
+config.loggerapp = {
+  hostName: 'localhost',
+  ip: '127.0.0.1',
+  port: 50051,
+};
+config.persist = {
+  type: 'file',
+};
+
+config.eureka = {
+  host: 'localhost',
+  port: 8761,
+};
 
 module.exports = config;
--- src/run.js
+++ src/run.js
@@ -1,6 +1,8 @@
 const grpc = require('grpc');
 const eventsProto = grpc.load('src/protos/events.proto');
 const handleEvent = require('./event/event-handler');
+const EurekaClient = require('./register-app');
+const appConfig = require('./config');
 
 const server = new grpc.Server();
 
@@ -10,6 +12,9 @@ server.addService(eventsProto.EventService.service, {
     callback(null, response);
   },
 });
-server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
-console.log('Server running at http://127.0.0.1:50051');
+server.bind(`${appConfig.loggerapp.hostName}:${appConfig.loggerapp.port}`,
+    grpc.ServerCredentials.createInsecure());
+EurekaClient.start();
+console.log('Server running at ' +
+`${appConfig.loggerapp.ip}:${appConfig.loggerapp.port}`);
 server.start();
